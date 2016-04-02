64eea0c6f243ab932957293193c34b049254a0a0
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-02 22:05:16 +0300

    update

--- IssueTrackingSystem/app/app.js
+++ IssueTrackingSystem/app/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('MyFirsApp', []);
 
 app.controller('View', function ($scope) {
-	$scope.Home = "This home page";
+	$scope.Home = "This test home page";
 	console.log($scope);
 });

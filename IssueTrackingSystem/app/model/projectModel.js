90d856bca853e8d4ccbb46830f77dc407a4300b6
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-02 21:38:20 +0300

    create project model

---
+++ IssueTrackingSystem/app/model/projectModel.js
@@ -0,0 +1,33 @@
+var projectData = {
+	leader : 'leader',	//the person responsible for the project
+	name : "projectName",
+	description : 'projectDescription',
+	projectKey : 'projectKey',	//sha512
+	issues : {},
+	priorities : {},
+	labels : {}
+};
+
+var ProjectModel = (function() {
+	function ProjectModel() {
+
+	}
+
+	ProjectModel.prototype.createProject = function(projectData) {
+
+	};
+
+	ProjectModel.prototype.editProject = function(projectData, projectKey) {
+
+	};
+
+	ProjectModel.prototype.deleteProject = function(projectKey, leader) {
+
+	};
+
+	return {
+		load: function () {
+			return new ProjectModel();
+		}
+	}
+})();

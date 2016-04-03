e61b2ec0b78c65802ff48b5de5b9cefc6c9a3626
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-03 23:59:42 +0300

    add router

--- IssueTrackingSystem/app/app.js
+++ IssueTrackingSystem/app/app.js
@@ -1,6 +1,29 @@
-var app = angular.module('SoftUniIssueTracker', []);
+var IssueTrackingSystem = angular.module('IssueTrackingSystem', ['ngRoute']);
 
-app.controller('View', function ($scope) {
-	$scope.Home = "This test home page";
+IssueTrackingSystem.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'index.html',
+			controller : 'Home'
+		})
+		.when('/dashboard', {
+		templateUrl : 'view/dashboard/index.html',
+		controller : 'Dashboard'
+		})
+		.when('/login', {
+			templateUrl : 'view/login/login.html',
+			controller : 'Login'
+		})
+		.when('/Register', {
+			templateUrl : 'view/login/register.html',
+			controller : 'Register'
+		})
+		.otherwise({
+			redirectTo : '/'
+		})
+}]);
+
+
+IssueTrackingSystem.controller('Home', ['$scope', function ($scope) {
 	console.log($scope);
-});
+}]);

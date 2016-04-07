01b190083bad9448153cfd71cd744459e7531248
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-08 00:09:48 +0300

    comment all

--- IssueTrackingSystem/app/model/issueModel.js
+++ IssueTrackingSystem/app/model/issueModel.js
@@ -1,42 +1,42 @@
-var issueData = {
-	title : "title of issue",
-	description : "description",
-	issueKey : "key",	//sha512
-	assignee : "assignee",
-	status : "status",
-	dueDate : "dueDate",
-	priority : "priority",
-	labels : "labels",
-	comments : {}
-};
-
-var IssueModel = (function() {
-	function IssueModel() {
-	}
-
-	IssueModel.prototype.createIssue = function(issueData) {
-
-	};
-
-	IssueModel.prototype.openIssue = function(issueKey, assignee) {
-		
-	};
-
-	IssueModel.prototype.closeIssue = function(issueKey, assignee) {
-
-	};
-
-	IssueModel.prototype.resolvedIssue = function(issueKey) {
-		
-	};
-
-	IssueModel.prototype.reopenIssue = function(issueKey) {
-
-	};
-
-	return {
-		load: function () {
-			return new IssueModel();
-		}
-	}
-})();
+// var issueData = {
+// 	title : "title of issue",
+// 	description : "description",
+// 	issueKey : "key",	//sha512
+// 	assignee : "assignee",
+// 	status : "status",
+// 	dueDate : "dueDate",
+// 	priority : "priority",
+// 	labels : "labels",
+// 	comments : {}
+// };
+//
+// var IssueModel = (function() {
+// 	function IssueModel() {
+// 	}
+//
+// 	IssueModel.prototype.createIssue = function(issueData) {
+//
+// 	};
+//
+// 	IssueModel.prototype.openIssue = function(issueKey, assignee) {
+//
+// 	};
+//
+// 	IssueModel.prototype.closeIssue = function(issueKey, assignee) {
+//
+// 	};
+//
+// 	IssueModel.prototype.resolvedIssue = function(issueKey) {
+//
+// 	};
+//
+// 	IssueModel.prototype.reopenIssue = function(issueKey) {
+//
+// 	};
+//
+// 	return {
+// 		load: function () {
+// 			return new IssueModel();
+// 		}
+// 	}
+// })();

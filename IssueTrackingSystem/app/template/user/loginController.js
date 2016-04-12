d8c12c6195c8018b45da01e45158122eb87bda06
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-12 22:48:01 +0300

    add logic in function login

--- IssueTrackingSystem/app/template/user/loginController.js
+++ IssueTrackingSystem/app/template/user/loginController.js
@@ -1,8 +1,25 @@
 (function() {
-	IssueTrackingSystem.controller('Login', ['$scope', 'requester', function ($scope, requester) {
-
+	IssueTrackingSystem.controller('Login', ['$scope', '$sessionStorage', '$http', '$q', 'BASEURL',
+			function ($scope, $sessionStorage, $http, $q, BASEURL) {
 		$scope.login = function login() {
-			requester.loginUser($scope.username, $scope.password);
+			var deferred = $q.defer();
+			$http({
+				method: 'POST',
+				url: BASEURL + 'users/login',
+				data: "username=" + $scope.username + "&password=" + $scope.password,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).then(function success(response) {
+				deferred.resolve(response.data);
+				console.log(response);
+				console.log($sessionStorage);
+				$sessionStorage.userName = response.data.userName;
+				$sessionStorage.userAccessToken = response.data.access_token;
+			}, function error(error) {
+				console.log(error);
+				console.log($sessionStorage);
+			});
+
+			return deferred.promise;
 		};
 
 	}]);

da209f04f658c528887f096e142cbc809b69e127
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-14 07:03:45 +0300

    complete login and complete redirect to login page if user not login

--- IssueTrackingSystem/app/app.js
+++ IssueTrackingSystem/app/app.js
@@ -1,47 +1,45 @@
 var IssueTrackingSystem = angular
 	.module('IssueTrackingSystem', ['ngRoute', 'ngStorage'])
-	.constant('BASEURL', 'http://softuni-social-network.azurewebsites.net/api/');
+	.constant('BASE_URL', 'http://softuni-social-network.azurewebsites.net/api/');
 
 IssueTrackingSystem.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
 			templateUrl : 'app/template/home.html',
-			controller : 'Home'
+			controller : 'Main'
 		})
 		.when('/login', {
 			templateUrl : 'app/template/user/login.html',
-			controller : 'Login'
+			controller : 'User'
 		})
-		.when('/signup', {
-			templateUrl : 'app/template/user/signup.html',
-			controller : 'SignUp'
-		})
-		.when('/edit-issue', {
-			templateUrl : 'app/template/edit-issue/edit-issue.html',
-			controller : 'EditIssue'
-		})
-		// .otherwise({
-		// 	redirectTo: '/home'
+		// .when('/signup', {
+		// 	templateUrl : 'app/template/user/signup.html',
+		// 	controller : 'User'
 		// })
+		// .when('/edit-issue', {
+		// 	templateUrl : 'app/template/edit-issue/edit-issue.html',
+		// 	controller : 'EditIssue'
+		// })
+		.otherwise({
+			redirectTo: '/'
+		})
 }]);
 
 
-IssueTrackingSystem.controller('Home', ['$scope', '$sessionStorage', '$location', '$http',
-	function ($scope, $sessionStorage, $location, $http) {
-
-		// $http({
-		// 	method: 'POST',
-		// 	url: 'http://softuni-social-network.azurewebsites.net/api/users/login',
-		// 	data: "username=" + "n1p3ha" + "&password=" + "666666",
-		// 	headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		// }).then(function success(response) {
-		// 	console.log(response);
-		// 	console.log($sessionStorage);
-		// 	$sessionStorage.userName = response.data.userName;
-		// 	$sessionStorage.userAccessToken = response.data.access_token;
-		// }, function error(error) {
-		// 	console.log(error);
-		// 	console.log($sessionStorage);
-		// });
+IssueTrackingSystem.controller('Main', ['$scope', 'authentication', '$sessionStorage', '$location',
+	function ($scope, authentication, $sessionStorage, $location) {
+		if(!$sessionStorage.user || !$sessionStorage.isAuthenticated) {
+			delete $sessionStorage.access_token;
+			delete $sessionStorage.token_type;
+			delete $sessionStorage.user;
+			$sessionStorage.isAuthenticated = false;
+			$scope.isAuthenticated = $sessionStorage.isAuthenticated;
+			$location.path('/login');
+		} else {
+			$sessionStorage.isAuthenticated = true;
+			$scope.isAuthenticated = $sessionStorage.isAuthenticated;
+		}
+		
+		
 
 }]);

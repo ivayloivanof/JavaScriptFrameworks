9ff086fb8e16bd966ce6b1e4882aa09bf9192de3
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-16 20:00:06 +0300

    change session variable

--- IssueTrackingSystem/app/template/user/userController.js
+++ IssueTrackingSystem/app/template/user/userController.js
@@ -7,10 +7,10 @@
                 authentication.loginUser(user)
                     .then(function (loggedUser) {
                         $sessionStorage.isAuthenticated = true;
-                        $sessionStorage.access_token = loggedUser.access_token;
-                        $sessionStorage.token_type = loggedUser.token_type;
-                        $sessionStorage.user = loggedUser.userName;
-                        $location.path('/');
+                        $sessionStorage.access_token = loggedUser.data.access_token;
+                        $sessionStorage.token_type = loggedUser.data.token_type;
+                        $sessionStorage.username = loggedUser.data.userName;
+                        $location.path('/login');
                     });
             };
 
@@ -18,16 +18,16 @@
                 authentication.registerUser(user)
                     .then(function (registeredUser) {
                         $sessionStorage.isAuthenticated = true;
-                        $sessionStorage.access_token = registeredUser.access_token;
-                        $sessionStorage.token_type = registeredUser.token_type;
-                        $sessionStorage.user = registeredUser.userName;
-                        $location.path('/');
+                        $sessionStorage.access_token = registeredUser.data.access_token;
+                        $sessionStorage.token_type = registeredUser.data.token_type;
+                        $sessionStorage.username = registeredUser.data.userName;
+                        $location.path('/register');
                     });
             };
 
             $scope.logoutUser = function () {
                 authentication.logout();
-                $location.path('/login');
+                $location.path('/logout');
             };
         }]);
 }());

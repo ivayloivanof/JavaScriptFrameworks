0c145807578292010a213edbca77e6aa2739853f
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-16 20:37:38 +0300

    add users service

---
+++ IssueTrackingSystem/app/template/user/users.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+    IssueTrackingSystem.factory('users', ['$http', '$q', 'BASE_URL', '$sessionStorage',
+        function ($http, $q, BASE_URL, $sessionStorage) {
+
+            function getAllUsers() {
+                var deferred = $q.defer();
+
+                $http({
+                    method: 'get',
+                    url: BASE_URL + 'users',
+                    headers: {
+                        'Authorization': $sessionStorage.token_type + ' ' + $sessionStorage.access_token
+                    }
+                }).then(function (success) {
+                    deferred.resolve(success);
+                }).then(function (error) {
+                    deferred.reject(error);
+                });
+
+                return deferred.promise;
+            }
+
+            return {
+                getAllUsers : getAllUsers
+            };
+        }]);
+}());

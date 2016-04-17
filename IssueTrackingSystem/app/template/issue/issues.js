d8008ed68aa28962dda5f14359a285b73d594a61
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-17 11:15:36 +0300

    change request for issue

--- IssueTrackingSystem/app/template/issue/issues.js
+++ IssueTrackingSystem/app/template/issue/issues.js
@@ -8,7 +8,7 @@
 
                 $http({
                     method: 'get',
-                    url: BASE_URL + 'issues/?filter=Priority.Name == "In Progress" or DueDate.Day <= 31&pageSize=1000&pageNumber=1',
+                    url: BASE_URL + 'issues/?filter=Status.Name == "In Progress" or DueDate.Day <= 31&pageSize=1000&pageNumber=1',
                     headers: {
                         'Authorization': $sessionStorage.token_type + ' ' + $sessionStorage.access_token
                     }

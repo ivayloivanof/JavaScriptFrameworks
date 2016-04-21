682ab587e1dadf5ea533f8397c7af5f17387f322
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-21 07:19:58 +0300

    create makeUserAdmin and changePassword functions

--- IssueTrackingSystem/app/template/user/users.service.js
+++ IssueTrackingSystem/app/template/user/users.service.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('IssueTrackingSystem.services.users', [])
-    .factory('users', ['$http', '$q', 'BASE_URL', 'header',
-        function ($http, $q, BASE_URL, header) {
+    .factory('users', ['$http', '$q', 'BASE_URL', 'header', '$sessionStorage',
+        function ($http, $q, BASE_URL, header, $sessionStorage) {
 
             function getAllUsers() {
                 var deferred = $q.defer();
@@ -35,49 +35,59 @@ angular.module('IssueTrackingSystem.services.users', [])
 
                 return deferred.promise;
             }
-            //
-            // function makeAdmin(userId) {
-            //     getCurrent().then(function (currentUser) {
-            //         if (!currentUser.isAdmin) {
-            //             console.error('Only admins can do that.');
-            //             return;
-            //         }
-            //
-            //         var deferred = $q.defer();
-            //         var data = 'UserId=' + userId;
-            //         $http.put(BASE_URL + 'users/makeadmin', data, headerService.getAuthAndWWWContentHeader())
-            //             .then(function (success) {
-            //                 deferred.resolve(success);
-            //             }, function (error) {
-            //                 deferred.reject(error);
-            //             });
-            //
-            //         return deferred.promise;
-            //     }, function (error) {
-            //         console.error(error);
-            //     });
-            // }
-            //
-            // function changePassword(oldPass, newPass, confirmNewPass) {
-            //     if (newPass != confirmNewPass) {
-            //         console.error('Passwords do not match.');
-            //         return;
-            //     }
-            //
-            //     var deferred = $q.defer();
-            //     var data = 'OldPassword=' + oldPass + '&NewPassword=' + newPass + '&ConfirmPassword=' + confirmNewPass;
-            //     $http.post(BASE_URL + 'api/Account/ChangePassword', data, headerService.getAuthAndWWWContentHeader())
-            //         .then(function (success) {
-            //             deferred.resolve(success);
-            //         }, function (error) {
-            //             deferred.reject(error);
-            //         });
-            //
-            //     return deferred.promise;
-            // }
+
+            function makeUserAdmin(userId) {
+                getCurrent().then(function () {
+                    if (!$sessionStorage.isAdmin) {
+                        console.error('Only admins can do that.');  //TODO Notification error
+                        return;
+                    }
+
+                    var deferred = $q.defer();
+                    var data = 'UserId=' + userId;
+                    $http({
+                        method: 'put',
+                        url: BASE_URL + 'users/makeadmin',
+                        data : data,
+                        headers: header.authenticationHeader() + header.getWWWContent()
+                    }).then(function (success) {
+                        deferred.resolve(success);
+                    }, function (error) {
+                        deferred.reject(error);
+                    });
+
+                    return deferred.promise;
+                }, function (error) {
+                    console.error(error);
+                });
+            }
+
+            function changePassword(oldPass, newPass, confirmNewPass) {
+                if (newPass != confirmNewPass) {
+                    console.error('Passwords do not match.');   //TODO notification
+                    return;
+                }
+
+                var deferred = $q.defer();
+                var data = 'OldPassword=' + oldPass + '&NewPassword=' + newPass + '&ConfirmPassword=' + confirmNewPass;
+                $http({
+                    method : 'post',
+                    url : BASE_URL + 'api/Account/ChangePassword',
+                    data : data,
+                    headers : header.authenticationHeader() + header.getWWWContent()
+                }).then(function (success) {
+                    deferred.resolve(success);
+                }, function (error) {
+                    deferred.reject(error);
+                });
+
+                return deferred.promise;
+            }
 
             return {
                 getAllUsers : getAllUsers,
-                getCurrentUser : getCurrentUser
+                getCurrentUser : getCurrentUser,
+                makeUserAdmin : makeUserAdmin,
+                changePassword : changePassword
             };
         }]);

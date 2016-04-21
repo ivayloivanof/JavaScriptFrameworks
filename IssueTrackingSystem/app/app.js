d7725f0df01a2f1e1acbd8d6143e1c5158b0db17
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-21 07:47:10 +0300

    add navbar directive

--- IssueTrackingSystem/app/app.js
+++ IssueTrackingSystem/app/app.js
@@ -11,6 +11,7 @@ angular.module('IssueTrackingSystem', [
     'IssueTrackingSystem.controllers.project',
     'IssueTrackingSystem.controllers.user',
     'IssueTrackingSystem.controllers.issue',
+    'IssueTrackingSystem.directives.navbar',
     'IssueTrackingSystem.directives.notification'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({

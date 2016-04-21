83001bc3d45ba17183964896ab659d906e1cf354
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-21 06:51:52 +0300

    refactoring debug mode

--- IssueTrackingSystem/app/template/issue/issue.controller.js
+++ IssueTrackingSystem/app/template/issue/issue.controller.js
@@ -1,15 +1,17 @@
 'use strict';
 
 angular.module('IssueTrackingSystem.controllers.issue', [])
-    .controller('Issue', ['$scope', 'issues', function ($scope, issues) {
+    .controller('Issue', ['$scope', 'debug', 'issues',
+        function ($scope, debug, issues) {
 
-        $scope.getAllIssues = function () {
-            issues.getAllIssues()
-                .then(function (issues) {
-                    console.log(issues);
-                    $scope.issues = issues.data.Issues;
-                });
-        };
+            $scope.getAllIssues = function () {
+                issues.getAllIssues()
+                    .then(function (issues) {
+                        //if debug mode is activated
+                        debug ? console.log('All issues:', issues) : '';
+                        $scope.issues = issues.data.Issues;
+                    });
+            };
 
-        $scope.getAllIssues();
-    }]);
+            $scope.getAllIssues();
+        }]);
--- IssueTrackingSystem/app/template/user/users.service.js
+++ IssueTrackingSystem/app/template/user/users.service.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('IssueTrackingSystem.services.users', [])
-    .factory('users', ['$http', '$q', 'BASE_URL', '$sessionStorage',
-        function ($http, $q, BASE_URL, $sessionStorage) {
+    .factory('users', ['$http', '$q', 'BASE_URL', 'header',
+        function ($http, $q, BASE_URL, header) {
 
             function getAllUsers() {
                 var deferred = $q.defer();
@@ -10,9 +10,7 @@ angular.module('IssueTrackingSystem.services.users', [])
                 $http({
                     method: 'get',
                     url: BASE_URL + 'users',
-                    headers: {
-                        'Authorization': $sessionStorage.token_type + ' ' + $sessionStorage.access_token
-                    }
+                    headers: header.authenticationHeader()
                 }).then(function (success) {
                     deferred.resolve(success);
                 }).then(function (error) {
@@ -22,7 +20,64 @@ angular.module('IssueTrackingSystem.services.users', [])
                 return deferred.promise;
             }
 
+            function getCurrentUser() {
+                var deferred = $q.defer();
+
+                $http({
+                    method: 'get',
+                    url: BASE_URL + 'users/me/',
+                    headers: header.authenticationHeader()
+                }).then(function (success) {
+                    deferred.resolve(success);
+                }, function (error) {
+                    deferred.reject(error);
+                });
+
+                return deferred.promise;
+            }
+            //
+            // function makeAdmin(userId) {
+            //     getCurrent().then(function (currentUser) {
+            //         if (!currentUser.isAdmin) {
+            //             console.error('Only admins can do that.');
+            //             return;
+            //         }
+            //
+            //         var deferred = $q.defer();
+            //         var data = 'UserId=' + userId;
+            //         $http.put(BASE_URL + 'users/makeadmin', data, headerService.getAuthAndWWWContentHeader())
+            //             .then(function (success) {
+            //                 deferred.resolve(success);
+            //             }, function (error) {
+            //                 deferred.reject(error);
+            //             });
+            //
+            //         return deferred.promise;
+            //     }, function (error) {
+            //         console.error(error);
+            //     });
+            // }
+            //
+            // function changePassword(oldPass, newPass, confirmNewPass) {
+            //     if (newPass != confirmNewPass) {
+            //         console.error('Passwords do not match.');
+            //         return;
+            //     }
+            //
+            //     var deferred = $q.defer();
+            //     var data = 'OldPassword=' + oldPass + '&NewPassword=' + newPass + '&ConfirmPassword=' + confirmNewPass;
+            //     $http.post(BASE_URL + 'api/Account/ChangePassword', data, headerService.getAuthAndWWWContentHeader())
+            //         .then(function (success) {
+            //             deferred.resolve(success);
+            //         }, function (error) {
+            //             deferred.reject(error);
+            //         });
+            //
+            //     return deferred.promise;
+            // }
+
             return {
-                getAllUsers : getAllUsers
+                getAllUsers : getAllUsers,
+                getCurrentUser : getCurrentUser
             };
         }]);

9579e961ffd32509c26b80d8579177b9fe93c3fc
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-21 06:03:58 +0300

    using header service for issues

--- IssueTrackingSystem/app/template/issue/issues.service.js
+++ IssueTrackingSystem/app/template/issue/issues.service.js
@@ -1,8 +1,8 @@
 "use strict";
 
 angular.module('IssueTrackingSystem.services.issues', [])
-    .factory('issues', ['$http', '$q', 'BASE_URL', '$sessionStorage',
-        function ($http, $q, BASE_URL, $sessionStorage) {
+    .factory('issues', ['$http', '$q', 'BASE_URL', 'header',
+        function ($http, $q, BASE_URL, header) {
 
             function getAllIssues() {
                 var deferred = $q.defer();
@@ -10,9 +10,7 @@ angular.module('IssueTrackingSystem.services.issues', [])
                 $http({
                     method: 'get',
                     url: BASE_URL + 'issues/?filter=Status.Name == "In Progress" or DueDate.Day <= 31&pageSize=1000&pageNumber=1',
-                    headers: {
-                        'Authorization': $sessionStorage.token_type + ' ' + $sessionStorage.access_token
-                    }
+                    headers: header.authenticationHeader()
                 }).then(function (success) {
                     deferred.resolve(success);
                 }).then(function (error) {

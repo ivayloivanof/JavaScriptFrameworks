2a21bbdd730e247afb42e263bdf3828f6b6d4289
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-21 06:03:37 +0300

    add header service

--- IssueTrackingSystem/app/app.js
+++ IssueTrackingSystem/app/app.js
@@ -2,14 +2,15 @@
 
 angular.module('IssueTrackingSystem', [
     'ngRoute', 'ngStorage', 'angular-loading-bar',
-    'IssueTrackingSystem.controllers.main',
-    'IssueTrackingSystem.controllers.project',
-    'IssueTrackingSystem.controllers.user',
-    'IssueTrackingSystem.controllers.issue',
+    'IssueTrackingSystem.services.header',
     'IssueTrackingSystem.services.projects',
     'IssueTrackingSystem.services.authentication',
     'IssueTrackingSystem.services.issues',
     'IssueTrackingSystem.services.users',
+    'IssueTrackingSystem.controllers.main',
+    'IssueTrackingSystem.controllers.project',
+    'IssueTrackingSystem.controllers.user',
+    'IssueTrackingSystem.controllers.issue',
     'IssueTrackingSystem.directives.notification'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({

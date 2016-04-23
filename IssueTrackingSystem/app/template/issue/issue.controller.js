ac38ab7524884d1b35dbf13c33fe49f7ead4d8e8
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-23 07:58:52 +0300

    add route and create getIssueById method; get issue and view

--- IssueTrackingSystem/app/template/issue/issue.controller.js
+++ IssueTrackingSystem/app/template/issue/issue.controller.js
@@ -1,8 +1,14 @@
 'use strict';
 
 angular.module('IssueTrackingSystem.controllers.issue', [])
-    .controller('Issue', ['$scope', 'debug', 'issues',
-        function ($scope, debug, issues) {
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/issue-view/:id', {
+            templateUrl: 'app/template/issue/issue-view.html',
+            controller: 'Issue'
+        });
+    }])
+    .controller('Issue', ['$scope', 'debug', 'issues', '$routeParams',
+        function ($scope, debug, issues, $routeParams) {
 
             $scope.getAllIssues = function () {
                 issues.getAllIssues('In Progress', 31, 1000, 1)
@@ -12,6 +18,14 @@ angular.module('IssueTrackingSystem.controllers.issue', [])
                         $scope.issues = issues.data.Issues;
                     });
             };
-
-            $scope.getAllIssues();
+            
+            $scope.getIssueById = function () {
+                console.log($routeParams);
+                issues.getIssuesById($routeParams.id)
+                    .then(function (issue) {
+                        //if debug mode is activated
+                        debug ? console.log('All issues:', issue) : '';
+                        $scope.issue = issue.data;
+                    });
+            };
         }]);

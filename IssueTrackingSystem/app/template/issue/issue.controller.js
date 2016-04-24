344f1c488882ac2fe281e4907ea727c85e52d7c6
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-24 06:54:40 +0300

    add route and create method for add-issue

--- IssueTrackingSystem/app/template/issue/issue.controller.js
+++ IssueTrackingSystem/app/template/issue/issue.controller.js
@@ -2,15 +2,19 @@
 
 angular.module('IssueTrackingSystem.controllers.issue', [])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/issue/:id', {
-            templateUrl: 'app/template/issue/issue-view.html',
-            controller: 'Issue'
-        });
+        $routeProvider
+            .when('/issue/:id', {
+                templateUrl: 'app/template/issue/issue-view.html',
+                controller: 'Issue'
+            }).when('/projects/:id/add-issue', {
+                templateUrl: 'app/template/issue/issue-add.html',
+                controller: 'Issue'
+            });
     }])
     .controller('Issue', ['$scope', 'debug', 'issues', '$routeParams',
         function ($scope, debug, issues, $routeParams) {
 
-            $scope.getAllIssues = function () {
+            $scope.getAllIssues = function getAllIssues() {
                 issues.getAllIssues('In Progress', 31, 10, 1)
                     .then(function (issues) {
                         //if debug mode is activated
@@ -19,7 +23,7 @@ angular.module('IssueTrackingSystem.controllers.issue', [])
                     });
             };
             
-            $scope.getIssueById = function () {
+            $scope.getIssueById = function getIssueById() {
                 issues.getIssuesById($routeParams.id)
                     .then(function (issue) {
                         //if debug mode is activated
@@ -29,13 +33,37 @@ angular.module('IssueTrackingSystem.controllers.issue', [])
                     });
             };
 
-            $scope.getIssueByProjectId = function () {
+            $scope.getIssueByProjectId = function getIssueByProjectId() {
                 issues.getIssuesByProjectId($routeParams.id)
                     .then(function (issues) {
                         //if debug mode is activated
                         debug ? console.log('Route params:', $routeParams) : '';
                         debug ? console.log('Issues:', issues) : '';
                         $scope.issues = issues.data;
+                    });
+            };
+
+            //admin or lead - for other not working
+            $scope.addIssue = function addIssue(issue) {
+
+                // build issue object
+                var issueCompleteObject = {
+                    Title: issue.title,
+                    Description: issue.description,
+                    DueDate: issue.dueDate,
+                    ProjectId: issue.projectId,
+                    AssigneeId: issue.assigneeId,
+                    PriorityId: issue.priorityId,
+                    Labels: []
+                };
+                issue.labels = issue.labels.split(',');
+                issue.labels.forEach(function (label) {
+                    issueCompleteObject.Labels.push({Name: label.trim()});
+                });
+
+                issues.addIssue(issueCompleteObject)
+                    .then(function (success) {
+                        console.log(success);
                     });
             };
         }]);

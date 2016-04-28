a90e2141d746b7b86660eec000c1e418cad8787f
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-28 07:16:29 +0300

    moved and refactoring code

--- IssueTrackingSystem/app/user/user.controller.js
+++ IssueTrackingSystem/app/user/user.controller.js
@@ -3,13 +3,14 @@
 angular.module('IssueTrackingSystem.controllers.user', [])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/profile/password', {
-            templateUrl: 'app/template/user/change-password.html',
+            templateUrl: 'app/user/partials/change-password.html',
             controller: 'User'
         });
     }])
     .controller('User', ['$scope', 'debug', '$location', 'authentication', '$sessionStorage', 'users',
         function user($scope, debug, $location, authentication, $sessionStorage, users) {
 
+            //completed
             $scope.loginUserInSystem = function (user) {
                 authentication.loginUser(user)
                     .then(function (loggedUser) {
@@ -35,6 +36,7 @@ angular.module('IssueTrackingSystem.controllers.user', [])
                     });
             };
 
+            //completed
             $scope.registerUserInSystem = function (user) {
                 authentication.registerUser(user)
                     .then(function (registeredUser) {
@@ -50,6 +52,7 @@ angular.module('IssueTrackingSystem.controllers.user', [])
                     });
             };
 
+            //completed
             $scope.changeUserPassword = function changePass(changeData) {
                 users.changePassword(changeData)
                     .then(function (changed) {
@@ -60,11 +63,13 @@ angular.module('IssueTrackingSystem.controllers.user', [])
                     });
             };
 
+            //completed
             $scope.logoutUser = function () {
                 authentication.logout();
                 $location.path('/logout');
             };
 
+            //completed
             $scope.hasLogged = function hasLogged() {
                 if ($sessionStorage.isAuthenticated) {
                     return true;
@@ -74,13 +79,7 @@ angular.module('IssueTrackingSystem.controllers.user', [])
                 $location.path('/');
             };
 
-            $scope.isAdmin = function isAdmin() {
-                if ($sessionStorage.isAdmin) {
-                    return true;
-                }
-                return false;
-            };
-
+            //completed
             $scope.getCurrentUser = function getCurrentUser() {
                 users.getCurrentUser()
                     .then(function (currentUser) {
@@ -88,6 +87,14 @@ angular.module('IssueTrackingSystem.controllers.user', [])
                         debug ? console.log('Current User:', currentUser) : '';
                         $scope.currentUser = currentUser.data;
                     });
+            };
+
+            //completed
+            $scope.isAdmin = function isAdmin() {
+                if ($sessionStorage.isAdmin) {
+                    return true;
+                }
+                return false;
             };
 
             $scope.isLead = function isLead(id) {

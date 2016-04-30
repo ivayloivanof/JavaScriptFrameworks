4cedb5fb0c5153a217a6efd657dc3ebc1ffb7ddd
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2016-04-30 15:37:53 +0300

    create error controller and page

---
+++ IssueTrackingSystem/app/error/error.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('IssueTrackingSystem.controllers.error', [])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/error', {
+            templateUrl: 'app/error/partials/error.html',
+            controller: 'Error'
+        });
+    }])
+    .controller('Error', ['$scope', '$sessionStorage',
+        function ($scope, $sessionStorage) {
+
+        }]);
